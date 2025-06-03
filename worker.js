3ceaf3b9dbc1ef0454f75486bf6dc955747e230f
AleksMG <5978497@gmail.com>
2025-06-03 18:57:27 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,24 +1,51 @@
-// Background worker for heavy crypto operations
-const crypto = new LatticeCrypto();
+importScripts('lattice.js');
+
+const crypto = new LatticeCrypto(64);
+
+// Convert text to binary and encrypt each bit
+function encryptText(publicKey, text) {
+    const bits = [];
+    for (let i = 0; i < text.length; i++) {
+        const byte = text.charCodeAt(i);
+        for (let j = 0; j < 8; j++) {
+            bits.push((byte >> (7 - j)) & 1);
+        }
+    }
+    return bits.map(bit => crypto.encryptBit(publicKey, bit));
+}
+
+// Decrypt bits and convert back to text
+function decryptText(secretKey, ciphertexts) {
+    const bits = ciphertexts.map(ct => crypto.decryptBit(secretKey, ct));
+    let text = '';
+    for (let i = 0; i < bits.length; i += 8) {
+        const byte = bits.slice(i, i + 8).reduce((acc, bit, j) => acc | (bit << (7 - j)), 0);
+        text += String.fromCharCode(byte);
+    }
+    return text;
+}
 
 self.onmessage = function(e) {
-    const { action, data, key } = e.data;
+    const { action, message, ciphertext, publicKey, secretKey } = e.data;
     
-    switch (action) {
-        case 'encrypt':
-            const encResult = crypto.encrypt(data, key);
-            self.postMessage({ 
-                ciphertext: encResult.ciphertext,
-                grid: encResult.grid 
-            });
-            break;
-            
-        case 'decrypt':
-            const decResult = crypto.decrypt(data, key);
-            self.postMessage({
-                plaintext: decResult.plaintext,
-                grid: decResult.grid
-            });
-            break;
+    try {
+        switch (action) {
+            case 'generateKeys':
+                const keys = crypto.generateKeys();
+                self.postMessage({ action, result: keys });
+                break;
+
+            case 'encrypt':
+                const encrypted = encryptText(publicKey, message);
+                self.postMessage({ action, result: encrypted });
+                break;
+
+            case 'decrypt':
+                const decrypted = decryptText(secretKey, JSON.parse(ciphertext));
+                self.postMessage({ action, result: decrypted });
+                break;
+        }
+    } catch (error) {
+        self.postMessage({ action, error: error.message });
     }
 };
